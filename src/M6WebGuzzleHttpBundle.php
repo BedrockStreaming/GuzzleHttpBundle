66ed52392bba819aa8f957d499bc02f54b2bd4c0
Stéphane Douzima <sdouzima.externe@bedrockstreaming.com>
2022-02-07 11:03:37 +0100

    fix: add needed missing return type

--- src/M6WebGuzzleHttpBundle.php
+++ src/M6WebGuzzleHttpBundle.php
@@ -4,17 +4,15 @@ namespace M6Web\Bundle\GuzzleHttpBundle;
 
 use M6Web\Bundle\GuzzleHttpBundle\DependencyInjection\MiddlewarePass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-/**
- * Class M6WebCassandraBundle
- */
 class M6WebGuzzleHttpBundle extends Bundle
 {
     /**
      * {@inheritdoc}
      */
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new DependencyInjection\M6WebGuzzleHttpExtension();
     }
